b3ab5cec29f81dfbbb248b9147e12932ea12921a
Amit <kumaramit710@gmail.com>
2021-08-07 19:58:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/elements/Navbar';
+import Contacts from './components/contacts/Contacts';
+import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
+import store from './store';
+
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <Router store={store}>
+       <div className="App">
+          <Navbar />
+          <div className="container">
+            <div className="py-3">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contacts/add" component={AddContact} />
+                <Route
+                  exact
+                  path="/contacts/edit/:id"
+                  component={EditContact}
+                />
+              </Switch>
+            </div>
+          </div>
+        </div>
+    </Router>
+    </Provider>
+    
   );
 }
 
