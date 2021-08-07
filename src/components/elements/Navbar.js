0d2b22423d7c946354f6e066c2a4c8b0855ed231
Amit <kumaramit710@gmail.com>
2021-08-07 20:17:06 +0530

    First commit

--- src/components/elements/Navbar.js
+++ src/components/elements/Navbar.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar shadow fixed-top navbar-expand-sm navbar-dark bg-primary">
+    <nav className="navbar shadow fixed-top navbar-expand-sm navbar-dark bg-warning">
       <div className="container">
         <Link to="/" className="navbar-brand">
           Contact Book
         </Link>
         <div>
           <Link to="/contacts/add" className="btn btn-light ml-auto">
-            Create Contact
+          <span class="material-icons mr-2">add</span> New Contact
           </Link>
         </div>
       </div>
